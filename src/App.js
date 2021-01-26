cb652eedfe375de92b74595f47bf0445dd36bcc7
Ivan Derlich <e@ivanderlich.com>
2021-01-26 13:52:45 -0300

    First deployment to heroku

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import fetch from 'node-fetch';
+
 
 function App() {
+  // eslint-disable-next-line no-useless-concat
+  const credentials = Buffer.from('Tu2pyn7ZhwuajNkMvAbUx4M7iU3ZHg_h' + ':' + 'oVQDFna_412dCcPz9Tf0FGjeL0YbKqxB').toString('base64');
+  console.log(credentials)
+  fetch('https://api.exchange.bitcoin.com/api/2/trading/balance', {
+    method: 'GET',
+    headers: {
+        'Authorization': 'Basic ' + credentials
+    }
+}).then(response => response.json())
+.then(info => console.log(info));;
   return (
     <div className="App">
       <header className="App-header">
