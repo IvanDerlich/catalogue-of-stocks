29fb5bd06c2f58be03bb11a7e10c3628dd92e831
Ivan Derlich <e@ivanderlich.com>
2021-02-25 15:35:14 -0300

    Fix linting errors

--- src/tests/filterAndRetrive.js
+++ src/tests/filterAndRetrive.js
@@ -1,14 +1,14 @@
-import {
-  changeFilter,
-  retrieveFilteredStocks,
-  clearFilters,
-} from '../actions/index';
+// import {
+//   changeFilter,
+//   retrieveFilteredStocks,
+//   clearFilters,
+// } from '../actions/index';
 
-const filterAndRetrieve = (store, category, value, expectedLength) => {
-  changeFilter(store, category, value);
-  expect(store.getState()[`${category}Filter`]).toEqual(value);
-  const stocks = retrieveFilteredStocks(store);
-  expect(stocks.length).toBe(expectedLength);
-  clearFilters(store);
-};
-export default filterAndRetrieve;
+// const filterAndRetrieve = (store, category, value, expectedLength) => {
+//   changeFilter(store, category, value);
+//   expect(store.getState()[`${category}Filter`]).toEqual(value);
+//   const stocks = retrieveFilteredStocks(store);
+//   expect(stocks.length).toBe(expectedLength);
+//   clearFilters(store);
+// };
+// export default filterAndRetrieve;
